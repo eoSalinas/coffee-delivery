c84ff6a6986223ba5ca9f8b7a70b3de675bc44b3
Salinas <matheussalinas3@gmail.com>
2022-10-17 01:21:05 -0300

    CopywritingContainer - Finished!:tada:

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,44 +1,46 @@
-import { CopywritingContainer, HomeContainer } from './style'
+import { Badge, CopywritingContainer, HomeContainer } from './style'
 import coffeHeadlineIMG from '../../assets/coffee-headline.svg'
 import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 
 export function Home() {
   return (
     <HomeContainer>
-      <CopywritingContainer>
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <h2>
-          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-          hora
-        </h2>
-        <ul>
-          <li>
-            <span>
-              <ShoppingCart size={16} weight="fill" color="#FFF" />
-            </span>{' '}
-            Compra simples e segura
-          </li>
-          <li>
-            <span>
-              <Timer size={16} weight="fill" color="#FFF" />
-            </span>{' '}
-            Entrega rápida e rastreada
-          </li>
-          <li>
-            <span>
-              <Package size={16} weight="fill" color="#FFF" />
-            </span>{' '}
-            Embalagem mantém o café intacto
-          </li>
-          <li>
-            <span>
-              <Coffee size={16} weight="fill" color="#FFF" />
-            </span>{' '}
-            O café chega fresquinho até você
-          </li>
-        </ul>
-      </CopywritingContainer>
-      <img src={coffeHeadlineIMG} alt="Café da Coffee Delivery" />
+      <div>
+        <CopywritingContainer>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <h2>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </h2>
+          <ul>
+            <li>
+              <Badge backgroundColor="yellow900">
+                <ShoppingCart size={16} weight="fill" color="#FFF" />
+              </Badge>{' '}
+              Compra simples e segura
+            </li>
+            <li>
+              <Badge backgroundColor="yellow500">
+                <Timer size={16} weight="fill" color="#FFF" />
+              </Badge>{' '}
+              Entrega rápida e rastreada
+            </li>
+            <li>
+              <Badge backgroundColor="brown300">
+                <Package size={16} weight="fill" color="#FFF" />
+              </Badge>{' '}
+              Embalagem mantém o café intacto
+            </li>
+            <li>
+              <Badge backgroundColor="purple500">
+                <Coffee size={16} weight="fill" color="#FFF" />
+              </Badge>{' '}
+              O café chega fresquinho até você
+            </li>
+          </ul>
+        </CopywritingContainer>
+        <img src={coffeHeadlineIMG} alt="Café da Coffee Delivery" />
+      </div>
     </HomeContainer>
   )
 }
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,15 +1,30 @@
 import styled from 'styled-components'
+import backgroundIMG from '../../assets/background-gradient.svg'
 
 export const HomeContainer = styled.main`
-  height: 544px;
+  padding: 0 10rem;
 
-  padding: 5.75rem 10rem;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  gap: 3.5rem;
+  justify-content: center;
+
+  div {
+    height: 544px;
+    width: 100vw;
+
+    padding: 0 10rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 3.5rem;
+
+    background-image: url(${backgroundIMG});
+    background-size: cover;
+  }
 `
-export const CopywritingContainer = styled.div`
+export const CopywritingContainer = styled.section`
   width: 36.75rem;
 
   h1 {
@@ -53,20 +68,32 @@ export const CopywritingContainer = styled.div`
       line-height: 130%;
 
       color: ${(props) => props.theme['brown-300']};
-
-      span {
-        width: 32px;
-        height: 32px;
-
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        margin-right: 0.75rem;
-
-        border-radius: 50%;
-
-        background-color: ${(props) => props.theme['purple-500']};
-      }
     }
   }
+`
+
+const BADGE_COLORS = {
+  yellow900: 'yellow-900',
+  yellow500: 'yellow-500',
+  brown300: 'brown-300',
+  purple500: 'purple-500',
+} as const
+
+interface BadgeProps {
+  backgroundColor: keyof typeof BADGE_COLORS
+}
+
+export const Badge = styled.span<BadgeProps>`
+  width: 32px;
+  height: 32px;
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 0.75rem;
+
+  border-radius: 50%;
+
+  background-color: ${(props) =>
+    props.theme[BADGE_COLORS[props.backgroundColor]]};
 `
