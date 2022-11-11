c2a9d60e931a4cb3c84452bc00f2512b1dcf1479
Salinas <matheussalinas3@gmail.com>
2022-11-11 16:20:02 -0300

    Added style for scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,34 +13,47 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     @media (max-width: 720px) {
-    font-size: 87.5%;
-  }
+      font-size: 87.5%;
+    }
 
-  body {
-    background-color: ${(props) => props.theme['gray-100']};
-    color: ${(props) => props.theme['brown-300']};
-    -webkit-font-smoothing: antialiased;
-  }
+    body {
+      background-color: ${(props) => props.theme['gray-100']};
+      color: ${(props) => props.theme['brown-300']};
+      -webkit-font-smoothing: antialiased;
+    }
 
-  body, input, textarea, button {
-    font: 400 1rem 'Roboto', sans-serif;
-  }
+    body, input, textarea, button {
+      font: 400 1rem 'Roboto', sans-serif;
+    }
 
-  h1, h2, h3, h4, h5, h6, strong {
-  font-weight: 700;
-}
+    h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+    }
 
-  a {
-  text-decoration: none;
-}
+    a {
+    text-decoration: none;
+    }
 
-  button {
-  cursor: pointer;
-}
+    button {
+    cursor: pointer;
+    }
 
-  [disable] {
-  opacity: 0.6;
-  cursor: not-allowed;
-}
+    [disable] {
+    opacity: 0.6;
+    cursor: not-allowed;
+    }
+
+    ::-webkit-scrollbar {
+      width: 0.4rem;
+    }
+    
+    ::-webkit-scrollbar-track {
+      background: ${({ theme }) => theme['gray-400']};
+    }
+    
+    ::-webkit-scrollbar-thumb {
+      border-radius: 2rem;
+      background: ${({ theme }) => theme['purple-500']};
+    }
   }
 `
